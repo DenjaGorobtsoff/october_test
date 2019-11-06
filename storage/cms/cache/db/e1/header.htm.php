117073812eaeb86c729ebdc4ad9fec7e891414f2
gorob4ik <denka.gorobtsoff@gmail.com>
2019-11-06 23:51:03 +0200

    october install files

---
+++ storage/cms/cache/db/e1/header.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5dc33aa5e78a6897934698_8727631ff1c46329496d12685c0274c0Class extends Cms\Classes\PartialCode
+{
+
+}
---
+++ storage/cms/cache/f1/94/default.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5dc33aa5cedf0394291432_429b540adb98137102855bdcdcddaf34Class extends Cms\Classes\LayoutCode
+{
+
+}
